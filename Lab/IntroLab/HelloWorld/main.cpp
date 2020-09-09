6bf6eccadf3197cebe3a5aacb9880043ce338b8c
Yash Tyagi <ytyagi1@student.rccd.edu>
2020-09-09 15:56:20 -0700

    Signed-off-by: Yash Tyagi <ytyagi1@student.rccd.edu>

--- Lab/IntroLab/HelloWorld/main.cpp
+++ Lab/IntroLab/HelloWorld/main.cpp
@@ -1,15 +1,33 @@
-/* Tyagi
- * Yash - IntroLab - 49285
+/* 
+ * File:   main.cpp
+ * Author: Yash Tyagi
+ * Created on August 26th, 2020, 2:40 PM
+ * Purpose:  HelloWorld Program for IntroLab Submission
  */
 
+//System Libraries
 #include <iostream>
 
 using namespace std;
 
-int main() {
+//Global Constants only
+//Math, Physics, Conversions, Higher Order Dimension 
 
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Set random number seed
+    
+    //Declare all variables
+    
+    //Initialize Variables
+
+    //Display Outputs
     cout << "Hello World";
+    
+    //Exist Stage Right
+    exit(0);
     
     return 0;
 }
-
